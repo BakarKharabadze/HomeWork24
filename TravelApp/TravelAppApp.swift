b3be559edfe1785c659280e3103130c0b39f753f
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-05-29 18:24:31 +0400

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Bakar Kharabadze on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
